2c3d689479176d8925cb1e6edbf0017c279b2ca2
Amit <amitkashyap02@gmail.com>
2018-04-14 19:27:32 +1000

    Changed POM file

--- src/main/java/com/inventoryapp/ItemController.java
+++
@@ -1,5 +0,0 @@
-package com.inventoryapp;
-
-public class ItemController {
-
-}
--- src/main/java/com/inventoryapp/Main.java
+++ src/main/java/com/inventoryapp/Main.java
@@ -1,6 +1,10 @@
 package com.inventoryapp;
 
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
 public class Main {
 public static void main(String args[]) {
+	
 	}
 }
