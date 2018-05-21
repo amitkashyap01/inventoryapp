3254e1feee08f5214d1beb50ed89e0a603e9a7d5
Amit Kumar Kashyap <amit_kashyap01@infosys.com>
2018-05-21 23:14:25 +1000

    Added ResponseEntity for addItem method

--- src/main/java/com/inventoryapp/controller/ItemController.java
+++ src/main/java/com/inventoryapp/controller/ItemController.java
@@ -9,13 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.inventoryapp.entity.Item;
 import com.inventoryapp.service.ItemService;
 
 @RestController
-@RequestMapping("/item")
+@RequestMapping("/items")
 public class ItemController {
 	@Autowired
 	private ItemService itemService;
@@ -33,14 +36,23 @@ public class ItemController {
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	@ResponseStatus(HttpStatus.OK)
-	public void deleteItem(@PathVariable("id") int id){
+	public ResponseEntity<?> deleteItem(@PathVariable("id") int id){
 		 itemService.deleteItem(id);
+		 
+		 // Returning HTTP 204
+		 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public void addItem(@RequestBody Item item){
+	public ResponseEntity<?> addItem(@RequestBody Item item, UriComponentsBuilder ucBuilder){
 		System.out.println("The value from request body: "+item);
 		itemService.addItem(item);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(ucBuilder.path("/items/{itemId}").buildAndExpand(item.getItemid()).toUri());
+		
+		return new ResponseEntity<>(headers, HttpStatus.CREATED);
+
 	}
 	
 	@RequestMapping(method=RequestMethod.PUT)
