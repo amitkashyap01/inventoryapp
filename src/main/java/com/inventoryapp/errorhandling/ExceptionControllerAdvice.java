21727c48e87cca04e1822d54058f6f0c317422cb
Amit Kumar Kashyap <amit_kashyap01@infosys.com>
2018-05-22 23:04:28 +1000

    Added generic exception handler

--- src/main/java/com/inventoryapp/errorhandling/ExceptionControllerAdvice.java
+++ src/main/java/com/inventoryapp/errorhandling/ExceptionControllerAdvice.java
@@ -1,5 +1,24 @@
 package com.inventoryapp.errorhandling;
 
-public class ExceptionControllerAdvice {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
+@ControllerAdvice
+public class ExceptionControllerAdvice extends RuntimeException{
+
+	
+	public ResponseEntity<ErrorMessage> genericExceptionHander(Exception exception){
+			ErrorMessage genericErrorMessage = new ErrorMessage();
+			
+			genericErrorMessage.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			genericErrorMessage.setErrorMessage("Generic Exception: Sorry, we encountered some issues while "
+					+ "handling your request");
+			
+			return new ResponseEntity<ErrorMessage>(genericErrorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
 }
+	
+	
