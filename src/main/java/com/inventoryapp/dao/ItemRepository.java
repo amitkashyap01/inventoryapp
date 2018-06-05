a626e9e9989571e79c47813d9b5fb1f36049da43
Amit Kumar Kashyap <amit_kashyap01@infosys.com>
2018-06-05 23:17:43 +1000

    updated exception controller, added dependency for eureka starter

--- src/main/java/com/inventoryapp/dao/ItemRepository.java
+++ src/main/java/com/inventoryapp/dao/ItemRepository.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Repository;
 
 import com.inventoryapp.entity.Item;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 @Repository
 public interface ItemRepository extends CrudRepository<Item, Integer>{
-
+	Optional<?> findByItemName(String itemName);
 }
--- src/main/java/com/inventoryapp/errorhandling/ExceptionControllerAdvice.java
+++ src/main/java/com/inventoryapp/errorhandling/ExceptionControllerAdvice.java
@@ -19,6 +19,7 @@ public class ExceptionControllerAdvice extends RuntimeException{
 	}
 	
 	
+	
 }
 	
 	
